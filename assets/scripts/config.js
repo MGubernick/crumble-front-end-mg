abdfbdef510ca0724e71b13fb0707026d4e88173
MGubernick <MGubernick61@gmail.com>
2021-01-07 09:13:09 -0600

    Update config.js to include correct url for heroku
    - updated the config.js file to include the correct url from heroku

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://fathomless-brook-30890.herokuapp.com/',
+  production: 'https://fathomless-brook-30890.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
